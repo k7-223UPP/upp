ed11f710e015e4a3fbc7f6eff4e387aeab884ae2
k7-223UPP <k01.223.nrnumephi@gmail.com>
2015-11-29 18:20:10 +0300

    minor bug fix

--- task_page/views.py
+++ task_page/views.py
@@ -6,9 +6,12 @@ import task_library.task_reader
 from .forms import SubmissionDocument
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+import os
+from testing_system import process
+from upp import settings
 
 def handle_uploaded_file(f, strg):
-    with open('/Users/Misha/PycharmProjects/untitled folder/upp/sources/' + (str(strg) + ".cpp"), 'wb+') as destination:
+    with open(settings.BASE_DIR + os.sep + "sources" + os.sep + (str(strg) + ".cpp"), 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
 
@@ -17,10 +20,10 @@ def task_page(request, userPT_ID, section_ID, user_ID, task_ID):
     if request.method == 'POST':
         form = SubmissionDocument(request.POST, request.FILES)
         if form.is_valid():
-            userPT = get_object_or_404(UserPickedTask, id=userPT_ID)
-            SubmissionToSave = Submission(code_link = "look thisID.cpp", id_user = request.user ,id_task = userPT.id_task, id_section = userPT.id_section ,status = "PROGRESS")
-            SubmissionToSave.save()
-            handle_uploaded_file(request.FILES['docfile'], str(SubmissionToSave.id))
+            user_p_t = get_object_or_404(UserPickedTask, id=userPT_ID)
+            submission_to_save = Submission(code_link = "look thisID.cpp", id_user = request.user ,id_task = user_p_t.id_task, id_section = user_p_t.id_section ,status = process.STATUS_WAIT)
+            submission_to_save.save()
+            handle_uploaded_file(request.FILES['docfile'], str(submission_to_save.id))
             return HttpResponseRedirect(reverse('main'))
     else:
         context = {}
