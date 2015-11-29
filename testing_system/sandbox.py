2ccaf24d2cee4f0aa3a02b0ed153ebd4871472fb
Bidzilya Vladislav <vladb15@mail.ru>
2015-11-30 01:37:44 +0300

    yet another fix to .gitignore

--- testing_system/sandbox.py
+++ testing_system/sandbox.py
@@ -8,6 +8,25 @@ def set_limits(memory_limit):
     resource.setrlimit(resource.RLIMIT_AS, memory_limit, memory_limit + 1)
 
 
+def get_output(code_link, input_file_name, time_limit, memory_limit, test_number):
+    with open(input_file_name, 'r') as input_file:
+        process = subprocess.Popen(code_link, stdin=input_file, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=set_limits(memory_limit))
+        try:
+            out, err = process.communicate(timeout=time_limit)
+        except subprocess.TimeoutExpired:
+            process.kill()
+            raise verdict.TimeLimit(test_number)
+        except MemoryError:
+            process.kill()
+            raise verdict.MemoryLimit(test_number)
+
+        SUCCESSFUL_RETURN_CODE = 0
+        if process.returncode != SUCCESSFUL_RETURN_CODE:
+            raise verdict.RuntimeError(test_number)
+
+        return out
+
+
 def process_test(code_link, task_id, test_number):
     time_limit = float(task_reader.get_time_limit(task_id))
 
@@ -21,19 +40,3 @@ def process_test(code_link, task_id, test_number):
 
     with open(output_file_name, 'w') as output_file:
         output_file.write(output)
-
-
-def get_output(code_link, input_file_name, time_limit, memory_limit, test_number):
-    with open(input_file_name, 'r') as input_file:
-        process = subprocess.Popen(code_link, stdin=input_file, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=set_limits(memory_limit))
-        try:
-            out, err = process.communicate(timeout=time_limit)
-        except subprocess.TimeoutExpired:
-            process.kill()
-            raise verdict.TimeLimit(test_number)
-        except MemoryError:
-            process.kill()
-            raise verdict.MemoryLimit(test_number)
-        except RuntimeError:
-            raise verdict.RuntimeError(test_number)
-        return out
