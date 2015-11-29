d3224dbe70172c1f80ed91addc50d22a96ce1f46
Bidzilya Vladislav <vladb15@mail.ru>
2015-11-29 16:24:16 +0300

    added verdict class

---
+++ testing_system/verdict.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+class Verdict(object):
+    def __str__(self):
+        return '-------'
+
+class Accepted(Verdict):
+    def __str__(self):
+        return 'AC'
+
+class WrongAnswer(Verdict):
+    def __init__(self, test_number):
+        self.test_number = test_number
+
+    def __str__(self):
+        return 'WA ' + str(self.test_number)
+
+class MemoryLimit(Verdict):
+    def __init__(self, test_number):
+        self.test_number = test_number
+
+    def __str__(self):
+        return 'ML ' + str(self.test_number)
+
+class TimeLimit(Verdict):
+    def __init__(self, test_number):
+        self.test_number = test_number
+
+    def __str__(self):
+        return 'TL ' + str(self.test_number)
+
+class RuntimeError(Verdict):
+    def __init__(self, test_number):
+        self.test_number = test_number
+
+    def __str__(self):
+        return 'RE ' + str(self.test_number)
+
+class CompilationError(Verdict):
+    def __str__(self):
+        return 'CE'
