7e578e7ebf50c91115592f595f2f037db2648621
Bidzilya Vladislav <vladb15@mail.ru>
2015-12-20 19:11:38 +0300

    slowing down tl solution for 7 task

--- task_base/5/solutions/tl.cpp
+++ task_base/5/solutions/tl.cpp
@@ -16,9 +16,13 @@ int main() {
     int n;
     cin >> n;
     int ans = 0;
-    for (int i = 1; i <= n; ++i) {
-        if (GetGcd(i, n) == 1) {
-            ++ans;
+    for (int itr0 = 1; itr0 <= n; ++itr0) {
+        for (int itr1 = 1; itr1 <= n; ++itr1) {
+            for (int i = 1; i <= n; ++i) {
+                if (GetGcd(i, n) == 1) {
+                    ++ans;
+                }
+            }
         }
     }
     cout << ans << endl;
