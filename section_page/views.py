83607c86231d16f15ad4454e23316ea9f30b10be
EAkimutin <evgakimutin@gmail.com>
2016-03-17 17:03:31 +0300

    добавлен алгоритм подбора задачи

--- section_page/views.py
+++ section_page/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render, get_object_or_404
-from upp_app.models import Section, TaskInSection, Task, TestTaskInSection, TestTask, UserPickedTask, Submission, UserClosedTasks
+from upp_app.models import Section, TaskInSection, Task, TestTaskInSection, TestTask, UserPickedTask, Submission, UserClosedTasks, UserRatingInSection
 import task_library.task_reader
 import random
 from django.shortcuts import redirect
@@ -34,14 +34,24 @@ def section_page(request, section_id):
             user_closed_tasks = UserClosedTasks.objects.filter(id_user=request.user, id_section=section)
             solved_tasks_id = set(user_closed_tasks.values_list('id_task', flat=True))
             suitable_tasks_id = list(tasks_in_section_id - solved_tasks_id)
+            user_rating = UserRatingInSection.objects.get(id_user=request.user, id_section=section).rating
+
+            best_task, deviation = None, 10**10
+            for task in tasks_in_section:
+                current_deviation = task.rating - user_rating
+                if task.id_task.id in suitable_tasks_id:
+                    if abs(deviation) > abs(current_deviation):
+                        best_task, deviation = task, current_deviation
+                    elif abs(deviation) == abs(current_deviation) and current_deviation > 0:
+                        best_task, deviation = task, current_deviation
+
             if len(suitable_tasks_id) == 0:
                 context['no_tasks'] = True
             else:
                 context['button_name'] = 'Начать обучение'
         if request.POST:
             if context['button_name'] == 'Начать обучение':
-                selected_task_id = random.choice(suitable_tasks_id)
-                selected_task = get_object_or_404(Task, id=selected_task_id)
+                selected_task = best_task.id_task
                 userPT = UserPickedTask(id_section = section, id_user = request.user, id_task = selected_task)
                 userPT.save()
             return redirect('task_page', section_id, userPT.id_task.id)
