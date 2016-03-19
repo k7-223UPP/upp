2ff39077ec9a67e0c4722897d9abacc42d384236
PetrovAnatoly <anatoliy159@gmail.com>
2016-03-19 03:37:19 +0300

    rating

--- section_page/views.py
+++ section_page/views.py
@@ -12,7 +12,7 @@ def section_page(request, section_id):
     task_id = []
     test_tasks_in_sect = TestTaskInSection.objects.all().filter(id_section=section_id)
     for i in test_tasks_in_sect:
-        task_id.append(i.id_test_task);
+        task_id.append(i.id_test_task)
     for i in task_id:
         task = get_object_or_404(TestTask, id=i.id)
         test_tasks[task_library.task_reader.get_task_html(task.id)] = task_library.task_reader.get_tutorial_html(
@@ -40,18 +40,26 @@ def section_page(request, section_id):
             user_closed_tasks = UserClosedTasks.objects.filter(id_user=request.user, id_section=section)
             solved_tasks_id = set(user_closed_tasks.values_list('id_task', flat=True))
             suitable_tasks_id = list(tasks_in_section_id - solved_tasks_id)
+            user_rating = UserRatingInSection.objects.get(id_user=request.user, id_section=section).rating
+            best_task, deviation = None, 10**10
+            for task in tasks_in_section:
+                current_deviation = task.rating - user_rating
+                if task.id_task.id in suitable_tasks_id:
+                    if abs(deviation) > abs(current_deviation):
+                        best_task, deviation = task, current_deviation
+                    elif abs(deviation) == abs(current_deviation) and current_deviation > 0:
+                        best_task, deviation = task, current_deviation
             if len(suitable_tasks_id) == 0:
                 context['no_tasks'] = True
             else: 
                 context['button_name'] = 'Начать обучение'
         if request.POST:
             if context['button_name'] == 'Начать обучение':
-                selected_task_id = random.choice(suitable_tasks_id)
-                selected_task = get_object_or_404(Task, id=selected_task_id)
+                selected_task = best_task.id_task
                 userPT = UserPickedTask(id_section = section, id_user = request.user, id_task = selected_task)
                 userPT.save()
             return redirect('task_page', section_id, userPT.id_task.id)
-        else:
+        else: 
             return render(request, 'section_page/section_page.html', context)
     else:
         return render(request, 'section_page/section_page.html', context)
